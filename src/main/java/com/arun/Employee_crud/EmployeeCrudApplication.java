9819d1559ba72ff4b7f5e2108b6139d72aeab058
arunbharath1082 <arunbarath2000@gmail.com>
2024-07-24 10:49:35 +0530

    initial commit

---
+++ src/main/java/com/arun/Employee_crud/EmployeeCrudApplication.java
@@ -0,0 +1,13 @@
+package com.arun.Employee_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arun/Employee_crud/EmployeeCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arun.Employee_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
